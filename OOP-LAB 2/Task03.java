4895628289ed1c5d44f251732feb376c7ed1f2d8
Talhapsycho <49289031+Talhapsycho@users.noreply.github.com>
2019-04-16 00:23:22 +0500

    Add files via upload

---
+++ OOP-LAB 2/Task03.java
@@ -0,0 +1,11 @@
+class Task03 {
+	public static void main(String talha[]){
+	float a=2.5f;
+	float b=3f;
+	float c=4f;
+	float d=1f;
+	float e=5.2f;
+	System.out.println("The total sum is:"+(a+b+c+d+e));
+	System.out.println("The average is:"+(a+b+c+d+e)/5);
+	}
+}
---
+++ OOP-LAB 2/Task04.java
@@ -0,0 +1,8 @@
+class Task04{
+	public static void main(String talha[]){
+float radius=3;
+System.out.println("The circumference of the circle is:"+ (2*3.14*radius));
+System.out.println("The area of the circle is:"+(3.14*radius*radius));
+System.out.println("The diameter of the circle is:"+2*radius);
+	}
+}
