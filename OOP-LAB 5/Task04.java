b913cbfaa87c52814b4c7b3e812b1d96f8ba6bc7
Talhapsycho <49289031+Talhapsycho@users.noreply.github.com>
2019-05-05 17:45:13 +0500

    Add files via upload

---
+++ OOP-LAB 5/Task04.java
@@ -0,0 +1,16 @@
+class Task04{
+	String name;
+	String dept;
+	String rollno;
+	Task04(String name,String dept,String rollno){
+		this.name=name;
+		this.dept=name;
+		this.rollno=rollno;
+		System.out.print(name+" "+dept+" "+rollno);
+
+
+	}
+	public static void main(String talha[]){
+    Task04 s1=new Task04("Talha","Software-Engineering","18SW37");
+}
+	}
